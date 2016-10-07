6d3cf6c3bbab922b6cbfffce433e37cf3fffded6
Dave Brookshire <dave@brookshire.org>
2016-10-07 14:18:43 -0500

    Update with new test scripts.

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 ## Synopsis
 
-pypicamcapper is a script which uses the pycamera module to periodcally capture images from a raspberry pi camera module.
+pypicamcapper is a script which uses the pycamera module to periodcally 
+capture images from a raspberry pi camera module.
 
 ## Motivation
 
-This script provides part of the solution I'm building to capture images of my wedding to build timelapse photo/videos 
-after the day is over.  I am going to deploy battery powered Raspberry Pi computers around the hall/room, which will run 
-this script, capturing images every 'TBD' seconds.  These images will be POSTed to an HPE Edgeline EL20 via wireless, since 
-the Pis will not have a lot of local storage.
+This script provides part of the solution I'm building to capture images 
+of my wedding to build timelapse photo/videos after the day is over.  
+I am going to deploy battery powered Raspberry Pi computers around the 
+hall/room, which will run this script, capturing images every 'TBD' 
+seconds.  These images will be POSTed to an HPE Edgeline EL20 via 
+wireless, since the Pis will not have a lot of local storage.
 
 ## Installation
 
@@ -19,7 +22,8 @@ TBD
 
 ## Contributors
 
-It's on GitHub, so...well, if you're really interested, the normal tools apply.
+It's on GitHub, so...well, if you're really interested, the normal 
+tools apply.
 
 ## License
 
---
+++ led_press3.py
@@ -0,0 +1,85 @@
+#
+#  Copyright (C) 2016 David Brookshire <dave@brookshire.org>
+#
+import time
+
+import RPi.GPIO as GPIO
+
+BUTTON_GPIO = 4
+LED1_GPIO = 17
+LED2_GPIO = 18
+
+
+class Lamp():
+    state = False  # True==On, False==Off
+    gpio = None
+
+    def __init__(self, gpio):
+        self.gpio = gpio
+        GPIO.setup(self.gpio, GPIO.OUT)
+
+    def __repr__(self):
+        return "LAMP-%d" % self.gpio
+
+    def on(self):
+        GPIO.output(self.gpio, True)
+
+    def off(self):
+        GPIO.output(self.gpio, False)
+
+
+class Button():
+    gpio = None
+
+    def __init__(self, gpio):
+        self.gpio = gpio
+        GPIO.setup(BUTTON_GPIO, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    def __repr__(self):
+        return "BUTTON-%d" % self.gpio
+
+    def get_state(self):
+        return GPIO.input(self.gpio)
+
+
+def do_blink(lamp, count=1, delay=0.5):
+    while count:
+        lamp.on()
+        time.sleep(delay)
+        lamp.off()
+        time.sleep(0.2)
+        count -= 1
+
+
+def do_shutdown(*leds):
+    if len(leds) >= 2:
+        do_blink(leds[0], 4)
+        do_blink(leds[1], 1, 4)
+    else:
+        do_blink(leds[0], 4)
+        do_blink(leds[0], 1, 4)
+
+    for l in leds:
+        l.off()
+
+if __name__ == '__main__':
+
+    # GPIO.cleanup()
+    GPIO.setmode(GPIO.BCM)
+
+    lamp1 = Lamp(LED1_GPIO)
+    lamp2 = Lamp(LED2_GPIO)
+    trigger = Button(BUTTON_GPIO)
+
+    try:
+        while True:
+            input_state = trigger.get_state()
+
+            if not input_state:
+                break
+
+    except KeyboardInterrupt:
+        pass
+
+    do_shutdown(lamp1, lamp2)
+    GPIO.cleanup()
